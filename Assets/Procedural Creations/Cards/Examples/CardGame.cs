c747a01b6c5aa5a2cdf808343a9fb6f07bcd3ca5
Hao Fu <fuhao8235@qq.com>
2016-12-02 11:47:32 +0800

    add original files

---
+++ Assets/Procedural Creations/Cards/Examples/CardGame.cs
@@ -0,0 +1,382 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardGame : MonoBehaviour
+{
+	public CardDeck Deck;
+	//List<CardDefinition> m_deck = new List<CardDefinition>();
+	
+	List<Card> m_dealer = new List<Card>();
+	List<Card> m_player = new List<Card>();
+
+	GameObject PlayerWins;
+	GameObject DealerWins;
+	GameObject NobodyWins;
+	
+	enum GameState
+	{
+		Invalid,
+		Started,
+		PlayerBusted,
+		Resolving,
+		DealerWins,
+		PlayerWins,
+		NobodyWins,
+	};
+
+	GameState m_state;
+	
+	GameObject [] Buttons;
+	
+	// Use this for initialization
+	void Start ()
+	{
+        //设置初始状态
+		m_state = GameState.Invalid;
+        //？？？Deck的意思应该就是卡牌的意思，但是这个初始化函数好像什么都没干
+		Deck.Initialize();
+        //获取找到三个Text的对象
+		PlayerWins = this.transform.Find("MessagePlayerWins").gameObject;
+		DealerWins = this.transform.Find("MessageDealerWins").gameObject;
+		NobodyWins = this.transform.Find("MessageTie").gameObject;
+        //将三个Text全都设置为隐藏
+		PlayerWins.SetActive(false);
+		DealerWins.SetActive(false);
+		NobodyWins.SetActive(false);
+        //初始化存放按键的Buttons数组
+		Buttons = new GameObject[3];
+        //获取找到三个Button的对象
+		Buttons[0] = this.transform.Find("Button1").gameObject;
+		Buttons[1] = this.transform.Find("Button2").gameObject;
+		Buttons[2] = this.transform.Find("Button3").gameObject;
+        //这个函数是用来更新button的颜色和状态，大约是在每一次需要更新button状态的时候调用
+		UpdateButtons();
+	}
+	//更新三个button的颜色状态
+	void UpdateButtons()
+	{
+		Buttons[0].GetComponent<Renderer>().material.color = Color.blue;
+		Buttons[1].GetComponent<Renderer>().material.color = (m_state == GameState.Started) ? Color.blue : Color.red;
+		Buttons[2].GetComponent<Renderer>().material.color = (m_state == GameState.Started || m_state == GameState.PlayerBusted) ? Color.blue : Color.red;
+	}
+	//显示获胜方信息
+	void ShowMessage(string msg)
+	{
+		if (msg == "Dealer")
+		{
+			PlayerWins.SetActive(false);
+			DealerWins.SetActive(true);
+			NobodyWins.SetActive(false);
+		}
+		else if (msg == "Player")
+		{
+			PlayerWins.SetActive(true);
+			DealerWins.SetActive(false);
+			NobodyWins.SetActive(false);
+		}
+		else if (msg == "Nobody")
+		{
+			PlayerWins.SetActive(false);
+			DealerWins.SetActive(false);
+			NobodyWins.SetActive(true);
+		}
+		else
+		{
+			PlayerWins.SetActive(false);
+			DealerWins.SetActive(false);
+			NobodyWins.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+    //就我的猜测这部分代码是想通过键盘控制按三个button
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			OnReset();
+		}
+		else if (Input.GetKeyDown(KeyCode.F2))
+		{
+			OnHitMe();
+		}
+		else if (Input.GetKeyDown(KeyCode.F3))
+		{
+			OnStop();
+		}
+		UpdateButtons();
+	}
+	//shuffle的话应该是洗牌的函数
+	void Shuffle()
+	{
+		if (m_state != GameState.Invalid)
+		{
+		}
+	}
+	//？？？清理卡牌函数
+	void Clear()
+	{
+        //依次销毁m_dealer list中的元素
+		foreach (Card c in m_dealer)
+		{
+			GameObject.DestroyImmediate(c.gameObject);
+		}
+        //依次销毁m_player list中的元素
+		foreach (Card c in m_player)
+		{
+			GameObject.DestroyImmediate(c.gameObject);
+		}
+        //清空List
+		m_dealer.Clear();
+		m_player.Clear();
+        //重新从卡牌库中添加卡牌
+		Deck.Reset();
+	}
+	
+	Vector3 GetDeckPosition()
+	{
+		return new UnityEngine.Vector3(-7,0,0);
+	}
+	
+	const float FlyTime = 0.5f;
+	
+	void HitDealer()
+	{
+        //从没用过的牌堆里取出一张牌
+		CardDef c1 = Deck.Pop();
+		if (c1 != null)
+		{
+			GameObject newObj = new GameObject();
+			newObj.name = "Card";
+			Card newCard = newObj.AddComponent(typeof(Card)) as Card;
+			newCard.Definition = c1;
+			newObj.transform.parent = Deck.transform;
+			newCard.TryBuild();
+			float x = -3+(m_dealer.Count)*2.0f;
+			float z = (m_dealer.Count)*-0.1f;
+			Vector3 deckPos = GetDeckPosition();
+			newObj.transform.position = deckPos;
+			m_dealer.Add(newCard);
+			newCard.SetFlyTarget(deckPos,new Vector3(x,2,z),FlyTime);
+		}
+	}
+	void HitPlayer()
+	{
+        Debug.Log("HitPlayer");
+        CardDef c1 = Deck.Pop();
+		if (c1 != null)
+		{
+            Debug.Log("Deck-Popped");
+            GameObject newObj = new GameObject();
+			newObj.name = "Card";
+			Card newCard = newObj.AddComponent(typeof(Card)) as Card;
+			newCard.Definition = c1;
+			newObj.transform.parent = Deck.transform;
+			newCard.TryBuild();
+			float x = -3+(m_player.Count)*1.5f;
+			float y = -3-m_player.Count*0.15f;
+			float z = (m_player.Count)*-0.1f;
+			//newObj.transform.position = new Vector3(x,-3,z);
+			m_player.Add(newCard);
+			Vector3 deckPos = GetDeckPosition();
+			newCard.transform.position = deckPos;
+			newCard.SetFlyTarget(deckPos,new Vector3(x,y,z),FlyTime);
+		}
+	}
+	
+	static int Value(Card c)
+	{
+		if (c != null)
+		{
+			switch (c.Definition.Pattern)
+			{
+			case 0:
+				return 10;
+			case 1:
+				return 11;
+			}
+			return c.Definition.Pattern;
+		}
+		return 0;
+	}
+	
+	static int GetScore(List<Card> cards)
+	{
+		int score = 0;
+		bool ace = false;
+		foreach (Card c in cards)
+		{
+			int s = Value(c);
+			if ((score + s) > 21)
+			{
+				if (s == 11)
+				{
+					s = 1;
+				}
+				else if (ace)
+				{
+					score -= 10;
+					ace = false;
+				}
+			}
+			score += s;
+			ace |= (s == 11);
+		}
+		return score;
+	}
+	
+	int GetDealerScore()
+	{
+		return GetScore(m_dealer);
+	}
+	
+	int GetPlayerScore()
+	{
+		return GetScore(m_player);
+	}
+	
+	const float DealTime = 0.35f;
+	
+    //按到Reset按键之后调用的函数
+	IEnumerator OnReset()
+	{
+        //就我的理解resolving指的状态应该是电脑控制发牌以及电脑Hit的状态
+		if (m_state != GameState.Resolving)
+		{
+			m_state = GameState.Resolving;
+            //不用显示谁获胜的提示口号
+			ShowMessage("");
+            //从新装满可用的牌
+            Clear();
+            //洗牌
+			Deck.Shuffle();
+			HitDealer();
+			yield return new WaitForSeconds(DealTime);
+			HitDealer();
+			yield return new WaitForSeconds(DealTime);
+			HitPlayer();
+			yield return new WaitForSeconds(DealTime);
+			HitPlayer();
+			m_state = GameState.Started;
+		}
+	}
+    //按到Hit按键之后调用的的函数
+	void OnHitMe()
+	{
+        Debug.Log("OnHitMe");
+		if (m_state == GameState.Started)
+		{
+			HitPlayer();
+			if (GetPlayerScore() > 21)
+			{
+				m_state = GameState.PlayerBusted;
+			}
+		}
+	}
+	bool TryFinalize(int playerScore, int dealer)
+	{
+		if (playerScore > 21)
+		{
+			// Dealer Wins!
+			ShowMessage("Dealer");
+			m_state = GameState.DealerWins;
+			return true;
+		}
+		if (dealer > 21)
+		{
+			ShowMessage("Player");
+			m_state = GameState.PlayerWins;
+			return true;
+		}
+		if (dealer > playerScore)
+		{
+			ShowMessage("Dealer");
+			m_state = GameState.DealerWins;
+			return true;
+		}
+		// Natural 21 beats everything else.
+		bool pn = (playerScore == 21) && (m_player.Count == 2);
+		bool dn = (dealer == 21) && (m_dealer.Count == 2);
+		if (pn && !dn)
+		{
+			ShowMessage("Player");
+			m_state = GameState.PlayerWins;
+			return true;
+		}
+		if (dn && !pn)
+		{
+			ShowMessage("Dealer");
+			m_state = GameState.DealerWins;
+			return true;
+		}
+		if (dealer > 17)
+		{
+			if (playerScore == dealer)
+			{
+				// Nobody Wins!
+				ShowMessage("Nobody");
+				m_state = GameState.NobodyWins;
+				return true;
+			}
+			else if (dealer < playerScore)
+			{
+				// Player Wins!
+				ShowMessage("Player");
+				m_state = GameState.PlayerWins;
+				return true;
+			}
+			else
+			{
+				// Dealer Wins!
+				ShowMessage("Dealer");
+				m_state = GameState.DealerWins;
+				return true;
+			}
+		}
+		return false;
+	}
+    //按到Stop按键之后调用的函数
+	IEnumerator OnStop()
+	{
+		if (m_state == GameState.Started || m_state == GameState.PlayerBusted)
+		{
+			m_state = GameState.Resolving;
+			UpdateButtons();
+			int playerScore = GetPlayerScore();
+			while (true)
+			{
+				int d = GetDealerScore();
+				Debug.Log(string.Format("Player={0}  Dealer={1}",playerScore,d));
+				if (TryFinalize(playerScore,d))
+				{
+					break;
+				}
+				else
+				{
+					Debug.Log("HitDealer");
+					HitDealer();
+					yield return new WaitForSeconds(DealTime*1.5f);
+				}
+			}
+		}
+	}
+	//这里的函数是提供给button外部调用的
+	public void OnButton(string msg)
+	{
+		Debug.Log("OnButton = "+msg);
+		switch (msg)
+		{
+		case "Reset":
+                //StartCoroutine()类似于开一个线程来进行这个函数
+			StartCoroutine(OnReset());
+			break;
+		case "Hit":
+			OnHitMe();
+			break;
+		case "Stop":
+			StartCoroutine(OnStop());
+			break;
+		}
+	}
+}
